0c472cf74df4b3575fbc18aa4b4a3565c73e7da7
shivaniN16 <shiva16n@gmail.com>
2017-01-19 10:53:18 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,11 +1,24 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='')
-def my_view(request):
-    return {'project': 'scaffolds'}
-@view_config(route_name='home', renderer='templates/login.pt')
-def my_view(request):
+#@view_config(route_name='home', renderer='templates/mytemplate.pt')
+#def my_view(request):
+ #   return {'project': 'scaffolds'}
+
+
+#@view_config(route_name='home', renderer='templates/login.pt')
+#def my_view(request):
+#    return {}
+
+
+@view_config(route_name='home', renderer='templates/hrview.pt')
+def hr_view(request):
     return {}
 
+@view_config(route_name='select', renderer='templates/selectstu.pt')
+def select_view(request):
+    return {}
 
+@view_config(route_name='eligible', renderer='templates/eligiblestu.pt')
+def elegible_view(request):
+    return {}
